0f1d08bf099cf74272999c5c7bbbd419d9fdf1cc
Paulo Castilho <32399161+castilhera@users.noreply.github.com>
2024-09-27 15:37:03 -0300

    db class adjustments

--- tasks/api/runtime/tasks_db.py
+++ tasks/api/runtime/tasks_db.py
@@ -3,11 +3,9 @@ import uuid
 
 class TasksDatabase():
 
-    _dynamodb = boto3.resource("dynamodb")
-
     def __init__(self, table_name: str):
+        self._dynamodb = boto3.resource("dynamodb")
         self._table = self._dynamodb.Table(table_name)
-
 
     def create_task(self, title: str, description: str, status: str):
         task = {
@@ -20,7 +18,6 @@ class TasksDatabase():
         self._table.put_item(Item=task)
 
         return task
-
 
     def update_task(self, task_id: str, title: str, description: str, status: str):
         update_expression = "set #title = :t, #description = :d, #status = :s"
@@ -43,11 +40,9 @@ class TasksDatabase():
             ExpressionAttributeNames=expression_attribute_names
         )
 
-
     def get_task(self, task_id: str):
         response = self._table.get_item(Key={"taskId": task_id})
         return response["Item"] if "Item" in response else None
-
 
     def delete_task(self, task_id: str) -> None:
         self._table.delete_item(Key={"taskId": task_id})
